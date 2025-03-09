c55505d73fd39cc398e58ee1de23d699075e9963
swain778 <swain.shub@gmail.com>
2025-03-09 17:50:34 +0530

    code update

--- handlers/userHandler.go
+++ handlers/userHandler.go
@@ -1,14 +1,17 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"restaurant-management/helpers"
 	"restaurant-management/models"
 	"restaurant-management/repository"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var userRepo = repository.UserRepository{}
@@ -69,4 +72,111 @@ func DeleteUser(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "User deleted successfully"})
+}
+
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req models.User
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		count, err := userRepo.CheckUser(req.Email)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while checking for the email"})
+			return
+		}
+		password := HashPassword(*req.Password)
+		req.Password = &password
+
+		count, err = userRepo.CheckPhone(req.Phone)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error while checking for the phone number"})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "this email and phone number already exist"})
+			return
+		}
+
+		token, refreshToken, _ := helpers.GenerateAllTokens(*req.Email, *req.FirstName, *req.LastName, req.UserId)
+		req.Token = &token
+		req.RefreshToken = &refreshToken
+
+		user := models.User{
+			ID:           uuid.New(),
+			FirstName:    req.FirstName,
+			LastName:     req.LastName,
+			Password:     req.Password,
+			Email:        req.Email,
+			Avatar:       req.Avatar,
+			Phone:        req.Phone,
+			Token:        req.Token,
+			RefreshToken: req.RefreshToken,
+			CreatedAt:    time.Now(),
+			UserId:       req.UserId,
+		}
+
+		if err := userRepo.CreateUser(&user); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+			log.Printf("Failed to create user: %v", err)
+			return
+		}
+
+	}
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var user models.User
+		var foundUser models.User
+
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err := userRepo.GetUserByEmail(user.Email)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "user not found"})
+			return
+		}
+
+		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+		if passwordIsValid != true {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		token, refreshToken, _ := helpers.GenerateAllTokens(*foundUser.Email, *foundUser.FirstName, *foundUser.LastName, foundUser.UserId)
+
+		_ = token
+		_ = refreshToken
+		//helpers.UpdateAllTokens(token, refreshToken, foundUser.UserId)
+
+		c.JSON(http.StatusOK, foundUser)
+	}
+}
+
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(bytes)
+}
+
+func VerifyPassword(userPassword string, providePassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providePassword), []byte(userPassword))
+	check := true
+	msg := ""
+
+	if err != nil {
+		msg = fmt.Sprintf("login or password is incorrect")
+		check = false
+	}
+	return check, msg
 }
--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,1 +1,75 @@
 package helpers
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+type SignedDetals struct {
+	Email     string
+	FirstName string
+	LastName  string
+	Uid       string
+	jwt.StandardClaims
+}
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email string, firstName string, lastName string, uid string) (signedToken string, efreshToken string, err error) {
+	claims := &SignedDetals{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+		Uid:       uid,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(20)).Unix(),
+		},
+	}
+	refreshClaims := &SignedDetals{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return token, refreshToken, err
+}
+
+// func UpdateAllTokens(signedToken string,signedRefreshToken string,userId string) {
+
+// }
+
+func ValidateToken(signedToken string) (claims *SignedDetals, msg string) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&SignedDetals{},
+		func(t *jwt.Token) (interface{}, error) {
+			return []byte(SECRET_KEY), nil
+		},
+	)
+
+	claims, ok := token.Claims.(*SignedDetals)
+	if !ok {
+		msg = fmt.Sprintf("the token is invalid")
+		msg = err.Error()
+		return
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		msg = fmt.Sprintf("token is expired")
+		msg = err.Error()
+		return
+	}
+	return claims, msg
+}
